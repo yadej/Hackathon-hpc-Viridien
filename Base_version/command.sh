b43103ceb24d61cd54ff9e3862f5b46b540561ac
etud14-2 <etud14-2@ip-10-0-14-252.us-east-2.compute.internal>
2025-01-20 15:12:56 +0100

    add template for testing

---
+++ Base_version/command.sh
@@ -0,0 +1,7 @@
+echo "Begin Computing"
+for i in {1..10};
+do
+	echo "Run $i"
+	./BSM 100 1000;
+done
+echo "End Computing"
---
+++ command_template.sh
@@ -0,0 +1,7 @@
+echo "Begin Computing"
+for i in {1..10};
+do
+	echo "Run $i"
+	./BSM 1 1000;
+done
+echo "End Computing"
